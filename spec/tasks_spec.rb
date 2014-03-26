35a37c55292cd7d7c94fe3c3f102ed20d52737a9
EmilyPlatzer <emilyplatzer@gmail.com>
2014-03-26 16:52:42 -0600

    User can add a task to the list of tasks on the homepage
    Refreshing page does not resubmit data

--- spec/tasks_spec.rb
+++ spec/tasks_spec.rb
@@ -6,6 +6,9 @@ Capybara.app = App
 feature "Managing tasks" do
   scenario "User sees a Welcome homepage" do
     visit '/'
-    expect(page).to have_content("Welcome")
+    click_on "Add a Task"
+    fill_in "task", with: "Redraft silk shirt pattern"
+    click_on "Create Task"
+    expect(page).to have_content("Redraft silk shirt pattern")
   end
 end
